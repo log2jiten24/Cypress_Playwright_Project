ee457cde5785fc4e3937eebde9a3242723551d86
KumarJitendra <=>
2024-01-08 17:30:17 +1100

    Git Hub Workflows_test

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,8 +30,10 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    screenshot: "only-on-failure",
+    video:"retain-on-failure"
   },
-
+  testMatch :["**.spec.ts"],
   /* Configure projects for major browsers */
   projects: [
     {
