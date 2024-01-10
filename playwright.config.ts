66beef35157a208fb059ffc314668628f73423d8
KumarJitendra <=>
2024-01-10 16:52:27 +1100

    Cypress_Config_Changes_Update

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     screenshot: "only-on-failure",
-    video:"retain-on-failure"
+    video:"on-first-retry"
   },
   testMatch :["**.spec.ts"],
   /* Configure projects for major browsers */
