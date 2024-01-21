a6aa93c46b5d1f6b0a95e37a3faeb35eb73fcf13
KumarJitendra <=>
2024-01-21 14:24:18 +1100

    Updated Config changes

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer tr */
     trace: 'on-first-retry',
     screenshot: "only-on-failure",
     video:"on-first-retry"
